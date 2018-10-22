569e6f467d22fceb934a88bdee14d4eb100b9bc2
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-10-21 20:20:26 -0400

    Remove reference to logo.svg

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import * as React from 'react';
 import IAppProps from './IAppProps';
 
 import './App.css';
-import logo from './logo.svg';
 
 class App extends React.Component<IAppProps> {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
