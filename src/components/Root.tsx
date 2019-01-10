7e78baa1f861191f083789b7421cbca3c74ed893
Jordan Brown <Cyberboss@users.noreply.github.com>
2019-01-10 15:43:01 -0500

    Setup navigation a little

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -6,11 +6,15 @@ import { Action, Dispatch } from "redux";
 import IServerClient from "../clients/IServerClient";
 
 import IRootState from "../store/IRootState";
+import PageType from "../store/PageType";
 
+import Home from "./Home";
 import Login from "./Login";
+import Navbar from "./Navbar";
 
 interface IStateProps {
   loggedIn: boolean;
+  pageType: PageType;
 }
 
 interface IOwnProps {
@@ -21,14 +25,38 @@ type IProps = IStateProps & IOwnProps;
 
 class Root extends React.Component<IProps> {
   public render(): React.ReactNode {
+    return (
+      <div className="Root">
+        {this.renderInterior()}
+      </div>
+    );
+  }
+
+  private renderInterior(): React.ReactNode {
     if (!this.props.loggedIn)
       return <Login serverClient={this.props.serverClient} />;
-    return <div />;
+
+    return (
+      <div className="Root-active">
+        <Navbar />
+        {this.renderPage()}
+      </div>
+    );
+  }
+
+  private renderPage(): React.ReactNode {
+    switch (this.props.pageType) {
+      case PageType.Home:
+        return <Home />;
+      default:
+        throw new Error("Invalid PageType!");
+    }
   }
 }
 
 const mapStateToProps = (state: IRootState, ownProps: IOwnProps): IStateProps => ({
-  loggedIn: state.loggedIn
+  loggedIn: state.loggedIn,
+  pageType: state.pageType
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<Action>, ownProps: IOwnProps): any => ({});
---
+++ src/store/PageType.ts
@@ -0,0 +1,20 @@
+enum PageType {
+    Home,
+    User,
+    CreateUser,
+    Admin,
+    Instance,
+    NewInstance,
+    InstanceSettings,
+    InstanceUser,
+    Repository,
+    Byond,
+    Compiler,
+    Watchdog,
+    ChatBots,
+    ChatBot,
+    NewChatBot,
+    StaticBrowser
+}
+
+export default PageType;
