9c676bb0116af922192675ab1c92c715a92c34fc
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2020-08-02 16:16:12 -0400

    Seperates the router from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
-import { FormattedMessage, IntlProvider } from "react-intl";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import { BrowserRouter } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
 import IAppProps from "./IAppProps";
@@ -10,68 +10,37 @@ import ITranslation from "./translations/ITranslation";
 import ITranslationFactory from "./translations/ITranslationFactory";
 import TranslationFactory from "./translations/TranslationFactory";
 
-import Login from "./components/views/Login";
 import AppNavbar from "./components/AppNavbar";
 
 import "./App.css";
-import loadable, { LoadableComponent } from "@loadable/component";
-import ErrorBoundary from "./components/utils/ErrorBoundary";
 import Loading from "./components/utils/Loading";
-import Reload from "./components/utils/Reload";
 import { StatusCode } from "./ApiClient/models/InternalComms/InternalStatus";
 import { ErrorCode } from "./ApiClient/models/InternalComms/InternalError";
 import ServerClient from "./ApiClient/ServerClient";
-import RouteController from "./utils/RouteController";
 import { getSavedCreds } from "./utils/misc";
-import { AppRoute } from "./utils/routes";
-import AccessDenied from "./components/utils/AccessDenied";
 import UserClient from "./ApiClient/UserClient";
+import Router from "./Router";
+
 interface IState {
     translation?: ITranslation;
     translationError?: string;
     loggedIn: boolean;
     loading: boolean;
     autoLogin: boolean;
-    routes: Array<AppRoute>;
 }
-
-const LoadSpin = (page: string) => (
-    <Loading text={"loading.page"}>
-        <FormattedMessage id={page} />
-    </Loading>
-);
-
-const NotFound = loadable(() => import("./components/views/NotFound"), {
-    fallback: LoadSpin("loading.page.notfound")
-});
 
 class App extends React.Component<IAppProps, IState> {
     private readonly translationFactory: ITranslationFactory;
-    private readonly components: Map<string, LoadableComponent<unknown>>;
 
     public constructor(props: IAppProps) {
         super(props);
 
         this.translationFactory = this.props.translationFactory || new TranslationFactory();
-        this.components = new Map<string, LoadableComponent<unknown>>();
-
-        const routes = RouteController.getImmediateRoutes(true, false);
-        routes.forEach(route => {
-            this.components.set(
-                route.name,
-                //*should* always be a react component
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                loadable(() => import(`./components/views/${route.file}`), {
-                    fallback: LoadSpin(route.name)
-                })
-            );
-        });
 
         this.state = {
             loggedIn: false,
             loading: true,
-            autoLogin: false,
-            routes: []
+            autoLogin: false
         };
     }
     public async componentDidMount(): Promise<void> {
@@ -88,15 +57,6 @@ class App extends React.Component<IAppProps, IState> {
         ServerClient.on("logout", () => {
             this.setState({
                 loggedIn: false
-            });
-        });
-        this.setState({
-            routes: await RouteController.getRoutes(true, false)
-        });
-
-        RouteController.on("refreshAll", routes => {
-            this.setState({
-                routes
             });
         });
 
@@ -153,38 +113,7 @@ class App extends React.Component<IAppProps, IState> {
                         ) : this.state.autoLogin && !this.state.loggedIn ? (
                             <Loading text="loading.app" />
                         ) : (
-                            <ErrorBoundary>
-                                <Reload>
-                                    <Switch>
-                                        {this.state.routes.map(route => {
-                                            if (!route.loginless && !this.state.loggedIn) return;
-
-                                            return (
-                                                <Route
-                                                    key={route.name}
-                                                    exact={!route.loose}
-                                                    path={route.route}
-                                                    render={props => {
-                                                        let Comp;
-
-                                                        if (!route.cachedAuth) {
-                                                            Comp = AccessDenied;
-                                                        } else {
-                                                            Comp = this.components.get(route.name)!;
-                                                        }
-
-                                                        //@ts-expect-error //i cant for the life of me make this shit work so it has to stay like this.
-                                                        return <Comp {...props} />;
-                                                    }}
-                                                />
-                                            );
-                                        })}
-                                        <Route>
-                                            {this.state.loggedIn ? <NotFound /> : <Login />}
-                                        </Route>
-                                    </Switch>
-                                </Reload>
-                            </ErrorBoundary>
+                            <Router loggedIn={this.state.loggedIn} />
                         )}
                     </Container>
                 </BrowserRouter>
---
+++ src/Router.tsx
@@ -0,0 +1,111 @@
+import { Route, Switch, withRouter } from "react-router-dom";
+import { Component, ReactNode } from "react";
+import { RouteComponentProps } from "react-router";
+import Reload from "./components/utils/Reload";
+import AccessDenied from "./components/utils/AccessDenied";
+import Login from "./components/views/Login";
+import ErrorBoundary from "./components/utils/ErrorBoundary";
+import * as React from "react";
+import RouteController from "./utils/RouteController";
+import { AppRoute } from "./utils/routes";
+import loadable, { LoadableComponent } from "@loadable/component";
+import Loading from "./components/utils/Loading";
+import { FormattedMessage } from "react-intl";
+
+interface IState {
+    loading: boolean;
+    routes: Array<AppRoute>;
+    components: Map<string, LoadableComponent<unknown>>;
+}
+interface IProps extends RouteComponentProps {
+    loggedIn: boolean;
+}
+
+const LoadSpin = (page: string) => (
+    <Loading text={"loading.page"}>
+        <FormattedMessage id={page} />
+    </Loading>
+);
+
+const NotFound = loadable(() => import("./components/views/NotFound"), {
+    fallback: LoadSpin("loading.page.notfound")
+});
+
+export default withRouter(
+    class Router extends Component<IProps, IState> {
+        public constructor(props: IProps) {
+            super(props);
+
+            const components = new Map<string, LoadableComponent<unknown>>();
+
+            const routes = RouteController.getImmediateRoutes(true, false);
+            routes.forEach(route => {
+                components.set(
+                    route.name,
+                    //*should* always be a react component
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+                    loadable(() => import(`./components/views/${route.file}`), {
+                        fallback: LoadSpin(route.name)
+                    })
+                );
+            });
+
+            this.state = {
+                loading: false,
+                routes: [],
+                components: components
+            };
+        }
+
+        public async componentDidMount() {
+            RouteController.on("refreshAll", routes => {
+                this.setState({
+                    routes
+                });
+            });
+
+            this.setState({
+                routes: await RouteController.getRoutes(true, false)
+            });
+        }
+
+        public render(): ReactNode {
+            return (
+                <ErrorBoundary>
+                    <Reload>
+                        <div>
+                            <Switch>
+                                {this.state.routes.map(route => {
+                                    if (!route.loginless && !this.props.loggedIn) return;
+
+                                    return (
+                                        <Route
+                                            exact={!route.loose}
+                                            path={route.route}
+                                            key={route.name}
+                                            render={props => {
+                                                let Comp;
+
+                                                if (!route.cachedAuth) {
+                                                    Comp = AccessDenied;
+                                                } else {
+                                                    Comp = this.state.components.get(route.name)!;
+                                                }
+
+                                                //@ts-expect-error //i cant for the life of me make this shit work so it has to stay like this.
+                                                return <Comp {...props} />;
+                                            }}
+                                        />
+                                    );
+                                })}
+                                <Route key="notfound">
+                                    {this.props.loggedIn ? <NotFound /> : <Login />}
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Reload>
+                </ErrorBoundary>
+            );
+        }
+    }
+);
