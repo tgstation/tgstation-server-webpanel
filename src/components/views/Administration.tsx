eeb958f45d192f4a6ce51ee0f92e8e05076c9f97
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2020-08-10 23:02:02 -0400

    Fixes a bug where the buttons in Administration couldn't be disabled

--- src/components/views/Administration.tsx
+++ src/components/views/Administration.tsx
@@ -14,10 +14,11 @@ import Modal from "react-bootstrap/Modal";
 import UserClient from "../../ApiClient/UserClient";
 import { AdministrationRights } from "../../ApiClient/generatedcode/_enums";
 import { FormattedMessage } from "react-intl";
-import { Link } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
+import { withRouter } from "react-router-dom";
 import { AppRoutes } from "../../utils/routes";
 
-interface IProps {}
+interface IProps extends RouteComponentProps {}
 interface IState {
     adminInfo?: Components.Schemas.Administration;
     serverInfo?: Components.Schemas.ServerInformation;
@@ -29,251 +30,251 @@ interface IState {
     showRebootModal?: boolean;
 }
 
-export default class Administration extends React.Component<IProps, IState> {
-    public constructor(props: IProps) {
-        super(props);
-        this.restart = this.restart.bind(this);
+export default withRouter(
+    class Administration extends React.Component<IProps, IState> {
+        public constructor(props: IProps) {
+            super(props);
+            this.restart = this.restart.bind(this);
 
-        this.state = {
-            busy: false,
-            canReboot: false,
-            canUpdate: false,
-            canLogs: false
-        };
-    }
-
-    public async componentDidMount(): Promise<void> {
-        this.setState({
-            busy: true
-        });
-        const tasks = [];
-
-        console.time("DataLoad");
-        tasks.push(this.loadAdminInfo());
-        tasks.push(this.loadServerInfo());
-        tasks.push(this.checkRebootRights());
-        tasks.push(this.checkUpdateRights());
-        tasks.push(this.checkLogsRights());
-
-        await Promise.all(tasks);
-        console.timeEnd("DataLoad");
-        this.setState({
-            busy: false
-        });
-    }
-
-    private async loadServerInfo() {
-        console.time("ServerLoad");
-        const response = await ServerClient.getServerInfo();
-        switch (response.code) {
-            case StatusCode.ERROR: {
-                this.setState({
-                    error: response.error
-                });
-                break;
-            }
-            case StatusCode.OK: {
-                this.setState({
-                    serverInfo: response.payload
-                });
-                break;
-            }
+            this.state = {
+                busy: false,
+                canReboot: false,
+                canUpdate: false,
+                canLogs: false
+            };
         }
-        console.timeEnd("ServerLoad");
-    }
 
-    private async loadAdminInfo() {
-        console.time("AdminLoad");
-        const response = await AdminClient.getAdminInfo();
-        switch (response.code) {
-            case StatusCode.ERROR: {
-                this.setState({
-                    error: response.error
-                });
-                break;
-            }
-            case StatusCode.OK: {
-                this.setState({
-                    adminInfo: response.payload
-                });
-                break;
-            }
-        }
-        console.timeEnd("AdminLoad");
-    }
-
-    private async checkRebootRights() {
-        const response = await UserClient.getCurrentUser();
-
-        if (response.code === StatusCode.OK) {
+        public async componentDidMount(): Promise<void> {
             this.setState({
-                canReboot: !!(
-                    response.payload!.administrationRights & AdministrationRights.RestartHost
-                )
+                busy: true
+            });
+            const tasks = [];
+
+            console.time("DataLoad");
+            tasks.push(this.loadAdminInfo());
+            tasks.push(this.loadServerInfo());
+            tasks.push(this.checkRebootRights());
+            tasks.push(this.checkUpdateRights());
+            tasks.push(this.checkLogsRights());
+
+            await Promise.all(tasks);
+            console.timeEnd("DataLoad");
+            this.setState({
+                busy: false
             });
         }
-    }
 
-    private async checkUpdateRights() {
-        const response = await UserClient.getCurrentUser();
-
-        if (response.code === StatusCode.OK) {
-            this.setState({
-                canReboot: !!(
-                    response.payload!.administrationRights & AdministrationRights.ChangeVersion
-                )
-            });
+        private async loadServerInfo() {
+            console.time("ServerLoad");
+            const response = await ServerClient.getServerInfo();
+            switch (response.code) {
+                case StatusCode.ERROR: {
+                    this.setState({
+                        error: response.error
+                    });
+                    break;
+                }
+                case StatusCode.OK: {
+                    this.setState({
+                        serverInfo: response.payload
+                    });
+                    break;
+                }
+            }
+            console.timeEnd("ServerLoad");
         }
-    }
 
-    private async checkLogsRights() {
-        const response = await UserClient.getCurrentUser();
-
-        if (response.code === StatusCode.OK) {
-            this.setState({
-                canLogs: !!(
-                    response.payload!.administrationRights & AdministrationRights.DownloadLogs
-                )
-            });
+        private async loadAdminInfo() {
+            console.time("AdminLoad");
+            const response = await AdminClient.getAdminInfo();
+            switch (response.code) {
+                case StatusCode.ERROR: {
+                    this.setState({
+                        error: response.error
+                    });
+                    break;
+                }
+                case StatusCode.OK: {
+                    this.setState({
+                        adminInfo: response.payload
+                    });
+                    break;
+                }
+            }
+            console.timeEnd("AdminLoad");
         }
-    }
 
-    private async restart() {
-        this.setState({
-            showRebootModal: false,
-            busy: true
-        });
-        console.time("Reboot");
-        const response = await AdminClient.restartServer();
-        switch (response.code) {
-            case StatusCode.ERROR: {
+        private async checkRebootRights() {
+            const response = await UserClient.getCurrentUser();
+
+            if (response.code === StatusCode.OK) {
                 this.setState({
-                    error: response.error
+                    canReboot: !!(
+                        response.payload!.administrationRights & AdministrationRights.RestartHost
+                    )
                 });
-                break;
-            }
-            case StatusCode.OK: {
-                window.location.reload();
             }
         }
-        this.setState({
-            busy: false
-        });
-        console.timeEnd("Reboot");
-    }
 
-    public render(): ReactNode {
-        if (this.state.busy) {
-            return <Loading text="loading.admin" />;
+        private async checkUpdateRights() {
+            const response = await UserClient.getCurrentUser();
+
+            if (response.code === StatusCode.OK) {
+                this.setState({
+                    canReboot: !!(
+                        response.payload!.administrationRights & AdministrationRights.ChangeVersion
+                    )
+                });
+            }
         }
 
-        const handleClose = () => this.setState({ showRebootModal: false });
-        const handleOpen = () => this.setState({ showRebootModal: true });
+        private async checkLogsRights() {
+            const response = await UserClient.getCurrentUser();
 
-        return (
-            <React.Fragment>
-                <ErrorAlert
-                    error={this.state.error}
-                    onClose={() => this.setState({ error: undefined })}
-                />
-                {this.state.adminInfo && this.state.serverInfo ? (
-                    <div className="text-center">
-                        <h3 className=" text-secondary">
-                            Host Machine OS:{" "}
-                            <FontAwesomeIcon
-                                fixedWidth
-                                icon={this.state.adminInfo.windowsHost ? faWindows : faLinux}
-                            />
-                        </h3>
-                        <h5 className="text-secondary">
-                            Remote repository:{" "}
-                            <a href={this.state.adminInfo.trackedRepositoryUrl!}>
-                                {this.state.adminInfo.trackedRepositoryUrl!}
-                            </a>
-                        </h5>
-                        <h3 className="text-secondary">
-                            Current version:{" "}
-                            <span
-                                className={
-                                    this.state.serverInfo.version! <
-                                    this.state.adminInfo.latestVersion!
-                                        ? "text-danger"
-                                        : ""
-                                }>
-                                {this.state.serverInfo.version!}
-                            </span>
-                        </h3>
-                        <h3 className="text-secondary">
-                            Latest version:{" "}
-                            <span
-                                className={
-                                    this.state.serverInfo.version! <
-                                    this.state.adminInfo.latestVersion!
-                                        ? "text-danger"
-                                        : ""
-                                }>
-                                {this.state.adminInfo.latestVersion!}
-                            </span>
-                        </h3>
-                        <hr />
-                        <Button
-                            className="mr-2"
-                            variant="danger"
-                            disabled={
-                                !(
-                                    this.state.canReboot &&
-                                    this.state.serverInfo.version! <
+            if (response.code === StatusCode.OK) {
+                this.setState({
+                    canLogs: !!(
+                        response.payload!.administrationRights & AdministrationRights.DownloadLogs
+                    )
+                });
+            }
+        }
+
+        private async restart() {
+            this.setState({
+                showRebootModal: false,
+                busy: true
+            });
+            console.time("Reboot");
+            const response = await AdminClient.restartServer();
+            switch (response.code) {
+                case StatusCode.ERROR: {
+                    this.setState({
+                        error: response.error
+                    });
+                    break;
+                }
+                case StatusCode.OK: {
+                    window.location.reload();
+                }
+            }
+            this.setState({
+                busy: false
+            });
+            console.timeEnd("Reboot");
+        }
+
+        public render(): ReactNode {
+            if (this.state.busy) {
+                return <Loading text="loading.admin" />;
+            }
+
+            const handleClose = () => this.setState({ showRebootModal: false });
+            const handleOpen = () => this.setState({ showRebootModal: true });
+
+            return (
+                <React.Fragment>
+                    <ErrorAlert
+                        error={this.state.error}
+                        onClose={() => this.setState({ error: undefined })}
+                    />
+                    {this.state.adminInfo && this.state.serverInfo ? (
+                        <div className="text-center">
+                            <h3 className=" text-secondary">
+                                Host Machine OS:{" "}
+                                <FontAwesomeIcon
+                                    fixedWidth
+                                    icon={this.state.adminInfo.windowsHost ? faWindows : faLinux}
+                                />
+                            </h3>
+                            <h5 className="text-secondary">
+                                Remote repository:{" "}
+                                <a href={this.state.adminInfo.trackedRepositoryUrl!}>
+                                    {this.state.adminInfo.trackedRepositoryUrl!}
+                                </a>
+                            </h5>
+                            <h3 className="text-secondary">
+                                Current version:{" "}
+                                <span
+                                    className={
+                                        this.state.serverInfo.version! <
                                         this.state.adminInfo.latestVersion!
-                                )
-                            }
-                            onClick={handleOpen}>
-                            <FormattedMessage id="view.admin.reboot.button" />
-                        </Button>
-                        <Button
-                            className="mr-2"
-                            variant="primary"
-                            disabled={
-                                !this.state.canUpdate &&
-                                this.state.serverInfo.version! < this.state.adminInfo.latestVersion!
-                            }
-                            as={Link}
-                            to={AppRoutes.admin_update.link || AppRoutes.admin_update.route}>
-                            <FormattedMessage id="view.admin.update.button" />
-                        </Button>
-                        <Button
-                            variant="primary"
-                            disabled={!this.state.canLogs}
-                            as={Link}
-                            to={AppRoutes.admin_logs.link || AppRoutes.admin_logs.route}>
-                            <FormattedMessage id="view.admin.logs.button" />
-                        </Button>
-                        <Modal
-                            show={this.state.showRebootModal}
-                            onHide={handleClose}
-                            size="lg"
-                            centered>
-                            <Modal.Header closeButton>
-                                <Modal.Title>
-                                    <FormattedMessage id="view.admin.reboot.modal.title" />
-                                </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <FormattedMessage id="view.admin.reboot.modal.body" />
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button onClick={handleClose}>
-                                    <FormattedMessage id="generic.close" />
-                                </Button>
-                                <Button variant="danger" onClick={this.restart}>
-                                    <FormattedMessage id="view.admin.reboot.button" />
-                                </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
-                ) : (
-                    ""
-                )}
-            </React.Fragment>
-        );
+                                            ? "text-danger"
+                                            : ""
+                                    }>
+                                    {this.state.serverInfo.version!}
+                                </span>
+                            </h3>
+                            <h3 className="text-secondary">
+                                Latest version:{" "}
+                                <span
+                                    className={
+                                        this.state.serverInfo.version! <
+                                        this.state.adminInfo.latestVersion!
+                                            ? "text-danger"
+                                            : ""
+                                    }>
+                                    {this.state.adminInfo.latestVersion!}
+                                </span>
+                            </h3>
+                            <hr />
+                            <Button
+                                className="mr-2"
+                                variant="danger"
+                                disabled={!this.state.canReboot}
+                                onClick={handleOpen}>
+                                <FormattedMessage id="view.admin.reboot.button" />
+                            </Button>
+                            <Button
+                                className="mr-2"
+                                variant="primary"
+                                disabled={!this.state.canUpdate}
+                                onClick={() => {
+                                    this.props.history.push(
+                                        AppRoutes.admin_update.link || AppRoutes.admin_update.route
+                                    );
+                                }}
+                                to={AppRoutes.admin_update.link || AppRoutes.admin_update.route}>
+                                <FormattedMessage id="view.admin.update.button" />
+                            </Button>
+                            <Button
+                                variant="primary"
+                                disabled={!this.state.canLogs}
+                                onClick={() => {
+                                    this.props.history.push(
+                                        AppRoutes.admin_logs.link || AppRoutes.admin_logs.route
+                                    );
+                                }}>
+                                <FormattedMessage id="view.admin.logs.button" />
+                            </Button>
+                            <Modal
+                                show={this.state.showRebootModal}
+                                onHide={handleClose}
+                                size="lg"
+                                centered>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>
+                                        <FormattedMessage id="view.admin.reboot.modal.title" />
+                                    </Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <FormattedMessage id="view.admin.reboot.modal.body" />
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button onClick={handleClose}>
+                                        <FormattedMessage id="generic.close" />
+                                    </Button>
+                                    <Button variant="danger" onClick={this.restart}>
+                                        <FormattedMessage id="view.admin.reboot.button" />
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                </React.Fragment>
+            );
+        }
     }
-}
+);
