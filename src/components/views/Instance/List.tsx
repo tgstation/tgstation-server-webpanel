f2a8b4a54ed0beaff8393a7fe29cf7b969d6502a
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2020-09-03 22:28:00 -0400

    Makes Instance/List use withRouter to be able to set the links in the future

--- src/components/views/Instance/List.tsx
+++ src/components/views/Instance/List.tsx
@@ -10,139 +10,152 @@ import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { AppCategories } from "../../../utils/routes";
+import { withRouter, RouteComponentProps } from "react-router-dom";
 
 interface IState {
     instances: Components.Schemas.Instance[];
     loading?: boolean;
     errors: Array<InternalError<ErrorCode> | undefined>;
+    //isnt directly used but is used to make react rerender when the selected insance is changed
+    instanceid?: number;
 }
-interface IProps {}
+interface IProps extends RouteComponentProps {}
 
-export default class InstanceList extends React.Component<IProps, IState> {
-    public constructor(props: IProps) {
-        super(props);
+export default withRouter(
+    class InstanceList extends React.Component<IProps, IState> {
+        public constructor(props: IProps) {
+            super(props);
 
-        this.state = {
-            loading: true,
-            instances: [],
-            errors: []
-        };
-    }
-
-    private addError(error: InternalError<ErrorCode>): void {
-        this.setState(prevState => {
-            const errors = Array.from(prevState.errors);
-            errors.push(error);
-            return {
-                errors
+            const actualid =
+                AppCategories.instance.data?.instanceid !== undefined
+                    ? parseInt(AppCategories.instance.data.instanceid as string)
+                    : undefined;
+            this.state = {
+                loading: true,
+                instances: [],
+                errors: [],
+                instanceid: actualid
             };
-        });
-    }
+        }
 
-    public async componentDidMount(): Promise<void> {
-        const instancelist = await InstanceClient.listInstances();
-        if (instancelist.code == StatusCode.OK) {
-            this.setState({
-                instances: instancelist.payload!
+        private addError(error: InternalError<ErrorCode>): void {
+            this.setState(prevState => {
+                const errors = Array.from(prevState.errors);
+                errors.push(error);
+                return {
+                    errors
+                };
             });
-        } else {
-            this.addError(instancelist.error!);
         }
 
-        this.setState({
-            loading: false
-        });
-    }
+        public async componentDidMount(): Promise<void> {
+            const instancelist = await InstanceClient.listInstances();
+            if (instancelist.code == StatusCode.OK) {
+                this.setState({
+                    instances: instancelist.payload!
+                });
+            } else {
+                this.addError(instancelist.error!);
+            }
 
-    public render(): ReactNode {
-        if (this.state.loading) {
-            return <Loading text="loading.instance.list" />;
+            this.setState({
+                loading: false
+            });
         }
 
-        return (
-            <div className="text-center">
-                <h3>
-                    <FormattedMessage id="view.instance.list.title" />
-                </h3>
-                {this.state.errors.map((err, index) => {
-                    if (!err) return;
-                    return (
-                        <ErrorAlert
-                            key={index}
-                            error={err}
-                            onClose={() =>
-                                this.setState(prev => {
-                                    const newarr = Array.from(prev.errors);
-                                    newarr[index] = undefined;
-                                    return {
-                                        errors: newarr
-                                    };
-                                })
-                            }
-                        />
-                    );
-                })}
-                <Table striped bordered hover variant="dark" responsive>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>
-                                <FormattedMessage id="generic.name" />
-                            </th>
-                            <th>
-                                <FormattedMessage id="generic.online" />
-                            </th>
-                            <th>
-                                <FormattedMessage id="generic.path" />
-                            </th>
-                            <th>
-                                <FormattedMessage id="generic.configmode" />
-                            </th>
-                            <th>
-                                <FormattedMessage id="generic.action" />
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.instances.map(value => {
-                            return (
-                                <tr key={value.id}>
-                                    <td>{value.id}</td>
-                                    <td>{value.name}</td>
-                                    <td>
-                                        {value.online! ? (
-                                            <Badge variant="success">
-                                                <FormattedMessage id="generic.online" />
-                                            </Badge>
-                                        ) : (
-                                            <Badge variant="danger">
-                                                <FormattedMessage id="generic.offline" />
-                                            </Badge>
-                                        )}
-                                    </td>
-                                    <td>{value.path}</td>
-                                    <td>
-                                        <FormattedMessage
-                                            id={`view.instance.configmode.${value.configurationType!.toString()}`}
-                                        />
-                                    </td>
-                                    <td className="align-middle p-0">
-                                        <Button
-                                            onClick={() => {
-                                                if (!AppCategories.instance.data)
-                                                    AppCategories.instance.data = {};
-                                                AppCategories.instance.data.instanceid = value.id.toString();
-                                            }}>
-                                            <FormattedMessage id="generic.edit" />
-                                        </Button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-                Selected instance: {AppCategories.instance.data?.instanceid}
-            </div>
-        );
+        public render(): ReactNode {
+            if (this.state.loading) {
+                return <Loading text="loading.instance.list" />;
+            }
+
+            return (
+                <div className="text-center">
+                    <h3>
+                        <FormattedMessage id="view.instance.list.title" />
+                    </h3>
+                    {this.state.errors.map((err, index) => {
+                        if (!err) return;
+                        return (
+                            <ErrorAlert
+                                key={index}
+                                error={err}
+                                onClose={() =>
+                                    this.setState(prev => {
+                                        const newarr = Array.from(prev.errors);
+                                        newarr[index] = undefined;
+                                        return {
+                                            errors: newarr
+                                        };
+                                    })
+                                }
+                            />
+                        );
+                    })}
+                    <Table striped bordered hover variant="dark" responsive>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>
+                                    <FormattedMessage id="generic.name" />
+                                </th>
+                                <th>
+                                    <FormattedMessage id="generic.online" />
+                                </th>
+                                <th>
+                                    <FormattedMessage id="generic.path" />
+                                </th>
+                                <th>
+                                    <FormattedMessage id="generic.configmode" />
+                                </th>
+                                <th>
+                                    <FormattedMessage id="generic.action" />
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.instances.map(value => {
+                                return (
+                                    <tr key={value.id}>
+                                        <td>{value.id}</td>
+                                        <td>{value.name}</td>
+                                        <td>
+                                            {value.online! ? (
+                                                <Badge variant="success">
+                                                    <FormattedMessage id="generic.online" />
+                                                </Badge>
+                                            ) : (
+                                                <Badge variant="danger">
+                                                    <FormattedMessage id="generic.offline" />
+                                                </Badge>
+                                            )}
+                                        </td>
+                                        <td>{value.path}</td>
+                                        <td>
+                                            <FormattedMessage
+                                                id={`view.instance.configmode.${value.configurationType!.toString()}`}
+                                            />
+                                        </td>
+                                        <td className="align-middle p-0">
+                                            <Button
+                                                onClick={() => {
+                                                    if (!AppCategories.instance.data)
+                                                        AppCategories.instance.data = {};
+                                                    AppCategories.instance.data.instanceid = value.id.toString();
+                                                    this.setState({
+                                                        instanceid: value.id
+                                                    });
+                                                }}>
+                                                <FormattedMessage id="generic.select" />
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                    Selected instance: {this.state.instanceid}
+                </div>
+            );
+        }
     }
-}
+);
