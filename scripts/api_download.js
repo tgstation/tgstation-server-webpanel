21479c81e1d6e53768b755060b836e23aa250267
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2020-12-30 17:14:48 -0500

    Adds the ability to override where the swagger json file is pulled from in the package json with 3 options: Github tag name, remote url or local file(absolute or relative to the package.json file)

--- scripts/api_download.js
+++ scripts/api_download.js
@@ -6,11 +6,37 @@ const pkg = require("../package.json");
 const file = fs.createWriteStream(
     path.resolve(__dirname, "..", "src", "ApiClient", "generatedcode", "swagger.json")
 );
-https.get(
-    "https://github.com/tgstation/tgstation-server/releases/download/api-v" +
-        pkg.tgs_api_version +
-        "/swagger.json",
-    function (response) {
-        response.pipe(file);
+
+switch (pkg.tgs_api.type) {
+    case "version": {
+        https.get(
+            "https://github.com/tgstation/tgstation-server/releases/download/api-v" +
+            pkg.tgs_api.value +
+            "/swagger.json",
+            function (response) {
+                response.pipe(file);
+            }
+        );
+        break;
     }
-);
+    case "url": {
+        https.get(
+            pkg.tgs_api.value,
+            function (response) {
+                response.pipe(file);
+            }
+        );
+        break;
+    }
+    case "file": {
+        let filepath;
+        if(path.isAbsolute(pkg.tgs_api.value)) {
+            filepath = pkg.tgs_api.value;
+        } else {
+            filepath = path.resolve(process.cwd(), pkg.tgs_api.value)
+        }
+
+        fs.createReadStream(filepath).pipe(file)
+        break;
+    }
+}
