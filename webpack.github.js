7438a84bcb489efa883e6aa826950ce187710d8b
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-01-29 19:14:06 -0500

    More temp

--- webpack.github.js
+++ webpack.github.js
@@ -1,10 +1,11 @@
 const createConfig = require("./webpack.common.js");
+const { version } = require("./package.json");
 
 //set TGS_WEBPANEL_GITHUB_PATH to the github pages subdir
 //set TGS_WEBPANEL_CLIENT_PATH to the path tgs will use to build
 //set GITHUB_SHA to the version commit
 
-process.env.TGS_WEBPANEL_GITHUB_PATH = "/tgstation-server-control-panel/";
+process.env.TGS_WEBPANEL_GITHUB_PATH = "http://localhost:3000/tgstation-server-webpanel/" + version + "/";
 process.env.TGS_WEBPANEL_CLIENT_PATH = "/app/";
 
 
