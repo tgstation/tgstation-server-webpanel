35768ce6daa470e953b9eac8c6e4f64d59327208
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-03-01 23:54:16 -0500

    File upload endpoint

--- src/ApiClient/TransferClient.ts
+++ src/ApiClient/TransferClient.ts
@@ -5,6 +5,10 @@ import InternalStatus, { StatusCode } from "./models/InternalComms/InternalStatu
 import ServerClient from "./ServerClient";
 
 export type DownloadErrors = GenericErrors | ErrorCode.TRANSFER_NOT_AVAILABLE;
+export type UploadErrors =
+    | GenericErrors
+    | ErrorCode.TRANSFER_NOT_AVAILABLE
+    | ErrorCode.UPLOAD_FAILED;
 
 export default new (class TransferClient extends ApiClient {
     public async Download(ticket: string): Promise<InternalStatus<string, DownloadErrors>> {
@@ -35,6 +39,64 @@ export default new (class TransferClient extends ApiClient {
                 return new InternalStatus({
                     code: StatusCode.OK,
                     payload: (response.data as unknown) as string
+                });
+            }
+            case 410: {
+                return new InternalStatus({
+                    code: StatusCode.ERROR,
+                    error: new InternalError(ErrorCode.TRANSFER_NOT_AVAILABLE, {
+                        errorMessage: response.data as Components.Schemas.ErrorMessageResponse
+                    })
+                });
+            }
+            default: {
+                return new InternalStatus({
+                    code: StatusCode.ERROR,
+                    error: new InternalError(
+                        ErrorCode.UNHANDLED_RESPONSE,
+                        { axiosResponse: response },
+                        response
+                    )
+                });
+            }
+        }
+    }
+
+    public async Upload(
+        ticket: string,
+        file: ArrayBuffer
+    ): Promise<InternalStatus<null, UploadErrors>> {
+        await ServerClient.wait4Init();
+
+        let response;
+        try {
+            response = await ServerClient.apiClient!.TransferController_Upload(
+                { ticket: ticket },
+                null,
+                {
+                    data: file
+                }
+            );
+        } catch (e) {
+            return new InternalStatus({
+                code: StatusCode.ERROR,
+                error: e as InternalError<GenericErrors>
+            });
+        }
+
+        switch (response.status) {
+            case 204: {
+                return new InternalStatus({
+                    code: StatusCode.OK,
+                    payload: null
+                });
+            }
+            case 409: {
+                return new InternalStatus({
+                    code: StatusCode.ERROR,
+                    error: new InternalError(ErrorCode.UPLOAD_FAILED, {
+                        void: true
+                    })
                 });
             }
             case 410: {
