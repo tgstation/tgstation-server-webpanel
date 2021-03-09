0fb3b97f8c849d18a593fb199f3b02a472cee6d4
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-03-09 12:15:46 -0500

    Haha big commit go brrrr: Adds a page for BYOND

--- src/ApiClient/ByondClient.ts
+++ src/ApiClient/ByondClient.ts
@@ -3,6 +3,7 @@ import { Components } from "./generatedcode/_generated";
 import InternalError, { ErrorCode, GenericErrors } from "./models/InternalComms/InternalError";
 import InternalStatus, { StatusCode } from "./models/InternalComms/InternalStatus";
 import ServerClient from "./ServerClient";
+import TransferClient, { UploadErrors } from "./TransferClient";
 
 export default new (class ByondClient extends ApiClient {
     public async getActiveVersion(
@@ -81,15 +82,16 @@ export default new (class ByondClient extends ApiClient {
 
     public async switchActive(
         instance: number,
-        version: string
-    ): Promise<InternalStatus<Components.Schemas.ByondInstallResponse, GenericErrors>> {
+        version: string,
+        file?: ArrayBuffer
+    ): Promise<InternalStatus<Components.Schemas.ByondInstallResponse, UploadErrors>> {
         await ServerClient.wait4Init();
 
         let response;
         try {
             response = await ServerClient.apiClient!.ByondController_Update(
                 { Instance: instance },
-                { version: version, uploadCustomZip: false }
+                { version: version, uploadCustomZip: !!file }
             );
         } catch (stat) {
             return new InternalStatus({
@@ -100,7 +102,37 @@ export default new (class ByondClient extends ApiClient {
 
         switch (response.status) {
             case 200:
-            case 204: {
+            case 202: {
+                const responseData = response.data as Components.Schemas.ByondInstallResponse;
+                if (responseData.fileTicket) {
+                    if (file) {
+                        const response2 = await TransferClient.Upload(
+                            responseData.fileTicket,
+                            file
+                        );
+                        if (response2.code === StatusCode.OK) {
+                            return new InternalStatus({
+                                code: StatusCode.OK,
+                                payload: responseData
+                            });
+                        } else {
+                            return new InternalStatus({
+                                code: StatusCode.ERROR,
+                                error: response2.error
+                            });
+                        }
+                    } else {
+                        return new InternalStatus({
+                            code: StatusCode.ERROR,
+                            error: new InternalError(ErrorCode.APP_FAIL, {
+                                jsError: Error(
+                                    "switchActive is uploading a custom zip without actually having a zip file to upload"
+                                )
+                            })
+                        });
+                    }
+                }
+
                 return new InternalStatus({
                     code: StatusCode.OK,
                     payload: response.data as Components.Schemas.ByondInstallResponse
