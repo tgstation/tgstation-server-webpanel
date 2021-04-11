39442d154d4ec1c3583b6cf39dad629e30d7c3b8
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-04-11 02:30:33 -0400

    Converts Hosting to use UserContext. This will also need an instance specific context

--- src/components/views/Instance/Hosting.tsx
+++ src/components/views/Instance/Hosting.tsx
@@ -12,7 +12,7 @@ import InstanceClient from "../../../ApiClient/InstanceClient";
 import InstancePermissionSetClient from "../../../ApiClient/InstancePermissionSetClient";
 import InternalError, { ErrorCode } from "../../../ApiClient/models/InternalComms/InternalError";
 import { StatusCode } from "../../../ApiClient/models/InternalComms/InternalStatus";
-import UserClient from "../../../ApiClient/UserClient";
+import { UserContext } from "../../../contexts/UserContext";
 import { GlobalObjects } from "../../../utils/globalObjects";
 import { resolvePermissionSet } from "../../../utils/misc";
 import { AppRoutes, RouteData } from "../../../utils/routes";
@@ -27,187 +27,177 @@ interface IState {
     loading: boolean;
     errors: Array<InternalError<ErrorCode> | undefined>;
     tab: string;
-    currentUser?: Components.Schemas.UserResponse;
     currentInstanceUser?: Components.Schemas.InstancePermissionSetResponse;
 }
 
-export default withRouter(
-    class Hosting extends React.Component<IProps, IState> {
-        public constructor(props: IProps) {
-            super(props);
+class Hosting extends React.Component<IProps, IState> {
+    public declare context: UserContext;
 
-            this.loadInstance = this.loadInstance.bind(this);
+    public constructor(props: IProps) {
+        super(props);
 
-            RouteData.instanceid = props.match.params.id;
-            RouteData.selectedinstancehostingtab = props.match.params.tab;
+        this.loadInstance = this.loadInstance.bind(this);
 
-            this.state = {
-                tab: props.match.params.tab || "byond",
-                errors: [],
-                loading: true
-            };
-        }
+        RouteData.instanceid = props.match.params.id;
+        RouteData.selectedinstancehostingtab = props.match.params.tab;
 
-        public componentDidUpdate(prevProps: Readonly<IProps>) {
-            if (prevProps.match.params.tab !== this.props.match.params.tab) {
-                this.setState({
-                    tab: this.props.match.params.tab || "byond"
-                });
-            }
-        }
+        this.state = {
+            tab: props.match.params.tab || "byond",
+            errors: [],
+            loading: true
+        };
+    }
 
-        public async componentDidMount() {
+    public componentDidUpdate(prevProps: Readonly<IProps>) {
+        if (prevProps.match.params.tab !== this.props.match.params.tab) {
             this.setState({
-                loading: true
+                tab: this.props.match.params.tab || "byond"
             });
-
-            const response = await UserClient.getCurrentUser();
-            if (response.code === StatusCode.OK) {
-                this.setState({
-                    currentUser: response.payload
-                });
-            } else {
-                this.addError(response.error);
-            }
-
-            await this.loadInstance();
-        }
-
-        private addError(error: InternalError<ErrorCode>): void {
-            this.setState(prevState => {
-                const errors = Array.from(prevState.errors);
-                errors.push(error);
-                return {
-                    errors
-                };
-            });
-        }
-
-        public async loadInstance() {
-            this.setState({
-                loading: true
-            });
-            const response = await InstanceClient.getInstance(parseInt(this.props.match.params.id));
-            if (response.code === StatusCode.OK) {
-                this.setState({
-                    instance: response.payload
-                });
-                const response2 = await InstancePermissionSetClient.getCurrentInstancePermissionSet(
-                    response.payload.id
-                );
-                if (response2.code === StatusCode.OK) {
-                    this.setState({
-                        currentInstanceUser: response2.payload
-                    });
-                } else {
-                    this.addError(response2.error);
-                }
-            } else {
-                this.addError(response.error);
-            }
-            this.setState({
-                loading: false
-            });
-        }
-
-        public render(): React.ReactNode {
-            if (this.state.loading) {
-                return <Loading text="loading.instance" />;
-            }
-
-            // noinspection DuplicatedCode
-            const changetabs = (newkey: string | null) => {
-                if (!newkey) return;
-
-                RouteData.instanceid = this.props.match.params.id;
-                RouteData.selectedinstancehostingtab = newkey;
-                if (!GlobalObjects.setupMode) {
-                    window.history.pushState(
-                        null,
-                        window.document.title,
-                        AppRoutes.instancehosting.link || AppRoutes.instancehosting.route
-                    );
-                }
-                this.setState({
-                    tab: newkey
-                });
-            };
-            const LoadSpin = <Loading text={"loading.page"} />;
-
-            //should always be a react component
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            const ByondPage = loadable(() => import(`./Hosting/Byond`), {
-                fallback: LoadSpin
-            });
-
-            return (
-                <div className="text-center">
-                    {this.state.errors.map((err, index) => {
-                        if (!err) return;
-                        return (
-                            <ErrorAlert
-                                key={index}
-                                error={err}
-                                onClose={() =>
-                                    this.setState(prev => {
-                                        const newarr = Array.from(prev.errors);
-                                        newarr[index] = undefined;
-                                        return {
-                                            errors: newarr
-                                        };
-                                    })
-                                }
-                            />
-                        );
-                    })}
-                    {this.state.instance ? (
-                        <h3>{`${this.state.instance.name} (${this.state.instance.id})`}</h3>
-                    ) : (
-                        <h3>
-                            <FormattedMessage id="generic.assert.noinstance" />
-                        </h3>
-                    )}
-                    <Button
-                        as={Link}
-                        to={AppRoutes.instancelist.link || AppRoutes.instancelist.route}>
-                        <FormattedMessage id="generic.goback" />
-                    </Button>
-                    {!this.state.instance ? (
-                        <FormattedMessage id="generic.assert.noinstance" />
-                    ) : !this.state.currentUser ? (
-                        <FormattedMessage id="generic.assert.nouser" />
-                    ) : !this.state.currentInstanceUser ? (
-                        <FormattedMessage id="generic.assert.nopermissionset" />
-                    ) : (
-                        <Tabs
-                            className="justify-content-center mb-3 mt-4 flex-column flex-md-row"
-                            activeKey={this.state.tab}
-                            onSelect={changetabs}>
-                            <Tab
-                                eventKey="byond"
-                                title={<FormattedMessage id="view.instance.hosting.byond" />}>
-                                <ByondPage
-                                    instance={this.state.instance}
-                                    selfPermissionSet={resolvePermissionSet(this.state.currentUser)}
-                                    selfInstancePermissionSet={this.state.currentInstanceUser}
-                                />
-                            </Tab>
-                            <Tab
-                                eventKey="users"
-                                title={
-                                    <FormattedMessage id="view.instance.config.instanceusers" />
-                                }>
-                                <WIPNotice />
-                            </Tab>
-                            <Tab
-                                eventKey="chatbots"
-                                title={<FormattedMessage id="view.instance.config.chatbots" />}>
-                                <WIPNotice />
-                            </Tab>
-                        </Tabs>
-                    )}
-                </div>
-            );
         }
     }
-);
+
+    public async componentDidMount() {
+        this.setState({
+            loading: true
+        });
+
+        await this.loadInstance();
+    }
+
+    private addError(error: InternalError<ErrorCode>): void {
+        this.setState(prevState => {
+            const errors = Array.from(prevState.errors);
+            errors.push(error);
+            return {
+                errors
+            };
+        });
+    }
+
+    public async loadInstance() {
+        this.setState({
+            loading: true
+        });
+        const response = await InstanceClient.getInstance(parseInt(this.props.match.params.id));
+        if (response.code === StatusCode.OK) {
+            this.setState({
+                instance: response.payload
+            });
+            const response2 = await InstancePermissionSetClient.getCurrentInstancePermissionSet(
+                response.payload.id
+            );
+            if (response2.code === StatusCode.OK) {
+                this.setState({
+                    currentInstanceUser: response2.payload
+                });
+            } else {
+                this.addError(response2.error);
+            }
+        } else {
+            this.addError(response.error);
+        }
+        this.setState({
+            loading: false
+        });
+    }
+
+    public render(): React.ReactNode {
+        if (this.state.loading) {
+            return <Loading text="loading.instance" />;
+        }
+
+        if (!this.context.user) {
+            throw Error("Hosting: this.context.user is null!");
+        }
+
+        // noinspection DuplicatedCode
+        const changetabs = (newkey: string | null) => {
+            if (!newkey) return;
+
+            RouteData.instanceid = this.props.match.params.id;
+            RouteData.selectedinstancehostingtab = newkey;
+            if (!GlobalObjects.setupMode) {
+                window.history.pushState(
+                    null,
+                    window.document.title,
+                    AppRoutes.instancehosting.link || AppRoutes.instancehosting.route
+                );
+            }
+            this.setState({
+                tab: newkey
+            });
+        };
+        const LoadSpin = <Loading text={"loading.page"} />;
+
+        //should always be a react component
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        const ByondPage = loadable(() => import(`./Hosting/Byond`), {
+            fallback: LoadSpin
+        });
+
+        return (
+            <div className="text-center">
+                {this.state.errors.map((err, index) => {
+                    if (!err) return;
+                    return (
+                        <ErrorAlert
+                            key={index}
+                            error={err}
+                            onClose={() =>
+                                this.setState(prev => {
+                                    const newarr = Array.from(prev.errors);
+                                    newarr[index] = undefined;
+                                    return {
+                                        errors: newarr
+                                    };
+                                })
+                            }
+                        />
+                    );
+                })}
+                {this.state.instance ? (
+                    <h3>{`${this.state.instance.name} (${this.state.instance.id})`}</h3>
+                ) : (
+                    <h3>
+                        <FormattedMessage id="generic.assert.noinstance" />
+                    </h3>
+                )}
+                <Button as={Link} to={AppRoutes.instancelist.link || AppRoutes.instancelist.route}>
+                    <FormattedMessage id="generic.goback" />
+                </Button>
+                {!this.state.instance ? (
+                    <FormattedMessage id="generic.assert.noinstance" />
+                ) : !this.state.currentInstanceUser ? (
+                    <FormattedMessage id="generic.assert.nopermissionset" />
+                ) : (
+                    <Tabs
+                        className="justify-content-center mb-3 mt-4 flex-column flex-md-row"
+                        activeKey={this.state.tab}
+                        onSelect={changetabs}>
+                        <Tab
+                            eventKey="byond"
+                            title={<FormattedMessage id="view.instance.hosting.byond" />}>
+                            <ByondPage
+                                instance={this.state.instance}
+                                selfPermissionSet={resolvePermissionSet(this.context.user)}
+                                selfInstancePermissionSet={this.state.currentInstanceUser}
+                            />
+                        </Tab>
+                        <Tab
+                            eventKey="users"
+                            title={<FormattedMessage id="view.instance.config.instanceusers" />}>
+                            <WIPNotice />
+                        </Tab>
+                        <Tab
+                            eventKey="chatbots"
+                            title={<FormattedMessage id="view.instance.config.chatbots" />}>
+                            <WIPNotice />
+                        </Tab>
+                    </Tabs>
+                )}
+            </div>
+        );
+    }
+}
+Hosting.contextType = UserContext;
+export default withRouter(Hosting);
