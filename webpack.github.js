9bfde33c54785e03cf6c84d3f7161217e31bf3d4
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-05-20 22:41:40 -0400

    Actually build for github, not my local environment.

--- webpack.github.js
+++ webpack.github.js
@@ -5,8 +5,8 @@ const { version } = require("./package.json");
 //set TGS_WEBPANEL_CLIENT_PATH to the path tgs will use to build
 //set GITHUB_SHA to the version commit
 
-process.env.TGS_WEBPANEL_GITHUB_PATH = "http://localhost:8000/";
-//process.env.TGS_WEBPANEL_GITHUB_PATH = "https://tgstation.github.io/tgstation-server-webpanel/webpanel/" + version + "/";
+//process.env.TGS_WEBPANEL_GITHUB_PATH = "http://localhost:8000/";
+process.env.TGS_WEBPANEL_GITHUB_PATH = "https://tgstation.github.io/tgstation-server-webpanel/webpanel/" + version + "/";
 process.env.TGS_WEBPANEL_CLIENT_PATH = "/app/";
 
 
