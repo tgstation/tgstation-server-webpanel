00325d9169ba5e769a3b88e4259e31602e49a15c
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2021-11-20 19:31:38 -0500

    Moves error details from JobCard s to a popup modal

--- src/components/utils/JobCard.tsx
+++ src/components/utils/JobCard.tsx
@@ -11,6 +11,7 @@ import { FormattedMessage, FormattedRelativeTime } from "react-intl";
 
 import { ErrorCode as TGSErrorCode } from "../../ApiClient/generatedcode/generated";
 import { TGSJobResponse } from "../../ApiClient/JobsClient";
+import JobError from "./JobError";
 
 interface IState {}
 interface IProps {
@@ -51,7 +52,7 @@ export default class JobCard extends React.Component<IProps, IState> {
                     className={`bg-${variant}`}>
                     #{job.id}: {job.description}
                 </ToastHeader>
-                <ToastBody className="pt-0">
+                <ToastBody className="pt-1 text-white">
                     {/*STAGE*/}
                     {job.stage ? <div className="mb-2">▶{job.stage}</div> : null}
                     {/*STARTED AT*/}
@@ -73,6 +74,13 @@ export default class JobCard extends React.Component<IProps, IState> {
                         )}
                     </OverlayTrigger>
                     <br />
+
+                    {/*ERROR*/}
+                    {job.errorCode !== undefined || job.exceptionDetails !== undefined ? (
+                        <JobError job={job} />
+                    ) : (
+                        ""
+                    )}
                     {/*CREATED BY*/}
                     <FormattedMessage id="app.job.startedby" />
                     <OverlayTrigger
@@ -136,27 +144,6 @@ export default class JobCard extends React.Component<IProps, IState> {
                                     </span>
                                 )}
                             </OverlayTrigger>
-                            <br />
-                        </React.Fragment>
-                    ) : (
-                        ""
-                    )}
-                    {(job.stoppedAt || job.cancelledBy) &&
-                    (job.errorCode !== undefined || job.exceptionDetails !== undefined) ? (
-                        <br />
-                    ) : (
-                        ""
-                    )}
-                    {/*ERROR*/}
-                    {job.errorCode !== undefined || job.exceptionDetails !== undefined ? (
-                        <React.Fragment>
-                            <span>
-                                <FormattedMessage id="view.instance.jobs.error" />(
-                                {job.errorCode !== undefined && job.errorCode !== null
-                                    ? TGSErrorCode[job.errorCode]
-                                    : "NoCode"}
-                                ): {job.exceptionDetails}
-                            </span>
                             <br />
                         </React.Fragment>
                     ) : (
---
+++ src/components/utils/JobError.tsx
@@ -0,0 +1,60 @@
+import ClickToSelect from "@mapbox/react-click-to-select";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FormattedMessage } from "react-intl";
+
+import { ErrorCode as TGSErrorCode } from "../../ApiClient/generatedcode/_enums";
+import { TGSJobResponse } from "../../ApiClient/JobsClient";
+
+interface IProps {
+    job: TGSJobResponse;
+}
+
+export default function JobError(props: IProps): JSX.Element {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            <Button
+                variant="danger"
+                className="d-block float-right"
+                onClick={() => setOpen(true)}
+                size="sm">
+                <FormattedMessage
+                    id="generic.details.info"
+                    values={{
+                        info:
+                            props.job.errorCode !== undefined && props.job.errorCode !== null
+                                ? TGSErrorCode[props.job.errorCode]
+                                : "NoCode"
+                    }}
+                />
+            </Button>
+
+            <Modal centered show={open} onHide={() => setOpen(false)} size="lg">
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        <FormattedMessage id={props.job.description} />
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="text-danger pb-0">
+                    <FormattedMessage id="view.instance.jobs.error" />:{" "}
+                    {props.job.errorCode !== undefined && props.job.errorCode !== null
+                        ? TGSErrorCode[props.job.errorCode]
+                        : "NoCode"}
+                    <hr />
+                    <ClickToSelect>
+                        <code className="bg-darker d-block pre-wrap p-2 pre-scrollable">
+                            {props.job.exceptionDetails}
+                        </code>
+                    </ClickToSelect>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setOpen(false)}>
+                        <FormattedMessage id="generic.close" />
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
