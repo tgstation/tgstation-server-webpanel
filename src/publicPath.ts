49c13205247b809ce5f184e00966f92c983cf76d
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2022-09-28 20:57:51 -0400

    Unbreaks the webpanel for tgs versions not supporting webpanel relocation Part 2

--- src/publicPath.ts
+++ src/publicPath.ts
@@ -1,10 +1,7 @@
 declare let __webpack_public_path__: string;
-declare const DEFAULT_BASEPATH: string;
-
-if (!window.publicPath) window.publicPath = DEFAULT_BASEPATH;
 
 // eslint-disable-next-line prefer-const
-__webpack_public_path__ = window.publicPath;
+if(window.publicPath) __webpack_public_path__ = window.publicPath;
 console.log("Public path:", __webpack_public_path__);
 
 export {};
