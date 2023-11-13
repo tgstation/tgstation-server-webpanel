ac396da7fdf5118bb3d68dd25c1866515174b056
Jordan Dominion <Cyberboss@users.noreply.github.com>
2023-11-13 18:35:06 -0500

    Delete unreferenced webpanel versions to reduce deployment size
    Need to automate this...

--- webpanel/4.21.3/service-worker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",(function(){self.skipWaiting()})),self.addEventListener("activate",(function(){self.registration.unregister().then((function(){return self.clients.matchAll()})).then((function(n){n.forEach((n=>n.navigate(n.url)))}))}));
