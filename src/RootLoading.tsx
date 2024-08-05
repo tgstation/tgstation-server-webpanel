7cf2f0cbc58daed4197d82e2fea9c9dc87879f8c
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-08-04 20:52:16 -0400

    Fix react-dom

--- src/RootLoading.tsx
+++ src/RootLoading.tsx
@@ -3,11 +3,12 @@ import React, { Suspense } from "react";
 const AppLoading = React.lazy(async () => import("./AppLoading"));
 
 export const RootLoading = (
-    <React.StrictMode>
-        <Suspense
-            fallback={
-                <React.Fragment>
-                    <style type="text/css">{`
+    <React.Fragment>
+        <React.StrictMode>
+            <Suspense
+                fallback={
+                    <React.Fragment>
+                        <style type="text/css">{`
                         body {
                             background: #212529;
                             color: #FFFFFF;
@@ -17,10 +18,11 @@ export const RootLoading = (
                             text-align: center;
                         }
                     `}</style>
-                    <p>Loading styles...</p>
-                </React.Fragment>
-            }>
-            <AppLoading />
-        </Suspense>
-    </React.StrictMode>
+                        <p>Loading styles...</p>
+                    </React.Fragment>
+                }>
+                <AppLoading />
+            </Suspense>
+        </React.StrictMode>
+    </React.Fragment>
 );
