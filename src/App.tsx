a55ae9bd701eb4910936193fb26ebaafed7149b2
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-09-29 14:55:04 -0400

    Set page title using helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 
-import Loading from './components/utils/Loading'
+import Loading from "./components/utils/Loading";
 
-import './App.css'
-
+import { Helmet } from "react-helmet";
+import Pkg from "../package.json";
 
 const App = () => {
-  return (
-    <StrictMode>
-        <Loading />
-    </StrictMode>
-  )
-}
+    return (
+        <StrictMode>
+            <Helmet>
+                <title>TGS Webpanel v{Pkg.version}</title>
+            </Helmet>
+            <Loading />
+        </StrictMode>
+    );
+};
 
-export default App
+export default App;
