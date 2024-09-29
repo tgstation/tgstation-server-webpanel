f8189ccf56046c572e705369d77a3ebbad4e0352
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-09-29 15:42:04 -0400

    Fleshing out app more

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 
 import Loading from "./components/utils/Loading/Loading";
 
 import { Helmet } from "react-helmet";
 import Pkg from "../package.json";
+import ITranslation from "./translations/ITranslation";
+import ITranslationFactory from "./translations/ITranslationFactory";
 
-const App = () => {
-    return (
-        <StrictMode>
+interface IProps {
+    locale: string;
+    translationFactory: ITranslationFactory;
+}
+
+const App = (props: IProps) => {
+    const [translations, setTranslations] = useState<ITranslation | null>(null);
+
+    // TODO
+    const [, setTranslationError] = useState<string | null>(null);
+
+    useEffect(() => {
+        void (async () => {
+            try {
+                const loadedTranslation =
+                    await props.translationFactory.loadTranslation(
+                        props.locale
+                    );
+                setTranslations(loadedTranslation);
+            } catch (error) {
+                setTranslationError(
+                    JSON.stringify(error) ?? "An unknown error occurred"
+                );
+            }
+        })();
+    }, [props.locale, props.translationFactory]);
+
+    const coreApp = (
+        <>
             <Helmet>
                 <title>TGS Webpanel v{Pkg.version}</title>
             </Helmet>
             <Loading />
-        </StrictMode>
+        </>
     );
+
+    return <StrictMode>{translations ? coreApp : <Loading />}</StrictMode>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import TranslationFactory from "./translations/TranslationFactory.ts";
 
 function mountApp() {
-    const root = createRoot(document.getElementById('root')!)
+    const root = createRoot(document.getElementById("root")!);
     root.render(
-        <App />,
-    )
+        <App
+            locale={navigator.languages[0]}
+            translationFactory={new TranslationFactory()}
+        />
+    );
 }
 
 window.addEventListener("DOMContentLoaded", mountApp);
-if (document.readyState === "interactive" || document.readyState === "complete") {
+if (
+    document.readyState === "interactive" ||
+    document.readyState === "complete"
+) {
     mountApp();
 }
