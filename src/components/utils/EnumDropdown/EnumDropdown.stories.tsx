9b36587e8003447c654dd296a45de12a14c0f0ab
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-10-04 17:48:53 -0400

    Increase delays for this test

--- src/components/utils/EnumDropdown/EnumDropdown.stories.tsx
+++ src/components/utils/EnumDropdown/EnumDropdown.stories.tsx
@@ -1,18 +1,18 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { userEvent, waitFor, within, expect, fn } from "@storybook/test";
+import { expect, fn, userEvent, waitFor, within } from "@storybook/test";
 
 import EnumDropdown from "./EnumDropdown";
 const config: Meta<typeof EnumDropdown> = {
     component: EnumDropdown,
     title: "Enum Dropdown",
     args: {
-        onChange: fn(),
-    },
+        onChange: fn()
+    }
 };
 
 enum TestEnum {
     A = "perms.group.warning",
-    B = "perms.group.create",
+    B = "perms.group.create"
 }
 
 export default config;
@@ -22,7 +22,7 @@ type Story = StoryObj<typeof config>;
 export const Default: Story = {
     args: {
         enum: TestEnum,
-        value: TestEnum.A,
+        value: TestEnum.A
     },
     play: async ({ args, canvasElement, step }) => {
         const canvas = within(canvasElement);
@@ -33,28 +33,26 @@ export const Default: Story = {
 
         await step("Select Create Item", async () => {
             const content = within(canvasElement.parentElement!);
-            await new Promise((resolve) => setTimeout(resolve, 100));
+            await new Promise(resolve => setTimeout(resolve, 200));
             await userEvent.click(content.getByTestId("dropdown-item-B"));
         });
 
         await waitFor(() => expect(args.onChange).toBeCalledWith(TestEnum.B));
-        await new Promise((resolve) => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 200));
 
         await step("Open Dropdown", async () => {
             await userEvent.click(canvas.getByTestId("dropdown-button"));
         });
 
-        await new Promise((resolve) => setTimeout(resolve, 100));
+        await new Promise(resolve => setTimeout(resolve, 200));
 
         await step("Select Warning Item", async () => {
             const content = within(canvasElement.parentElement!);
-            await new Promise((resolve) => setTimeout(resolve, 100));
+            await new Promise(resolve => setTimeout(resolve, 200));
             await userEvent.click(content.getByTestId("dropdown-item-A"));
         });
 
         await waitFor(() => expect(args.onChange).toBeCalledWith(TestEnum.A));
-    },
+    }
 };
-export const NoIntl = () => (
-    <EnumDropdown enum={TestEnum} noIntl value={TestEnum.A} />
-);
+export const NoIntl = () => <EnumDropdown enum={TestEnum} noIntl value={TestEnum.A} />;
