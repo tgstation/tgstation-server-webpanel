2f485b94ffa5ac4f7004ef2d338b8a7e9fcf596d
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-10-07 07:42:19 -0400

    Fix API grab script

--- src/api/grab.js
+++ src/api/grab.js
@@ -7,6 +7,6 @@ if (existsSync('src/api/schema.override')) {
     copyFileSync('src/api/schema.override', 'src/api/schema.graphql');
 } else {
     const stream = createWriteStream('src/api/schema.graphql');
-    const { body } = await fetch(`https://github.com/tgstation/tgstation-server/releases/graphql-v${Pkg.tgs_graphql_api_version}/tgs-api.graphql`);
+    const { body } = await fetch(`https://github.com/tgstation/tgstation-server/releases/download/graphql-v${Pkg.tgs_graphql_api_version}/tgs-api.graphql`);
     await finished(Readable.fromWeb(body).pipe(stream));
 }
