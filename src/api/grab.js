6e7ba2cc5bef462deb01c6d430fb28773aebe012
Jordan Dominion <Cyberboss@users.noreply.github.com>
2024-10-22 18:26:40 -0400

    Work on setting up relay for GitHub SxS

--- src/api/grab.js
+++ src/api/grab.js
@@ -3,10 +3,16 @@ import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 import Pkg from './../../package.json' with { type: 'json' };
 
-if (existsSync('src/api/schema.override')) {
-    copyFileSync('src/api/schema.override', 'src/api/schema.graphql');
-} else {
-    const stream = createWriteStream('src/api/schema.graphql');
-    const { body } = await fetch(`https://github.com/tgstation/tgstation-server/releases/download/graphql-v${Pkg.tgs_graphql_api_version}/tgs-api.graphql`);
-    await finished(Readable.fromWeb(body).pipe(stream));
+const DownloadSchema = async (src, dest) => {
+    const stream = createWriteStream(dest);
+    const { body } = await fetch(src);
+    return finished(Readable.fromWeb(body).pipe(stream));
 }
+
+if (existsSync('src/api/schema.override')) {
+    copyFileSync('src/api/schema.override', 'src/api/tgs_schema.graphql');
+} else {
+    await DownloadSchema(`https://github.com/tgstation/tgstation-server/releases/download/graphql-v${Pkg.tgs_graphql_api_version}/tgs-api.graphql`, 'src/api/tgs_schema.graphql');
+}
+
+await DownloadSchema('https://docs.github.com/public/fpt/schema.docs.graphql', 'src/api/github_schema.graphql');
